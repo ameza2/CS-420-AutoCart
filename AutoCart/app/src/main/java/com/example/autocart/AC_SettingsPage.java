c5309452b7f813d8131d5c44c733f4a5db15a8ec
Abraham Meza <ameza2@nevada.unr.edu>
2022-04-07 13:06:55 -0700

    Feedback/Support Page

--- AutoCart/app/src/main/java/com/example/autocart/AC_SettingsPage.java
+++ AutoCart/app/src/main/java/com/example/autocart/AC_SettingsPage.java
@@ -1,13 +1,46 @@
 package com.example.autocart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
 
 public class AC_SettingsPage extends AppCompatActivity {
+
+    Button settingsButton;
+    Button feedbackButton;
+    Button supportButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.autocart_settingspage);
+
+        settingsButton = (Button)findViewById(R.id.autocartSettings);
+        feedbackButton = (Button)findViewById(R.id.autocartFeedback);
+        supportButton = (Button)findViewById(R.id.autocartSupport);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(AC_SettingsPage.this, AC_ConfigPage.class);
+                startActivity(intent);
+            }
+        });
+
+        feedbackButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(AC_SettingsPage.this, AC_FeedbackPage.class);
+                startActivity(intent);
+            }
+        });
+
+        supportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(AC_SettingsPage.this, AC_SupportPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ AutoCart/app/src/main/java/com/example/autocart/AC_SupportPage.java
@@ -0,0 +1,13 @@
+package com.example.autocart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AC_SupportPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.autocart_supportpage);
+    }
+}
