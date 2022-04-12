ec9b6074b4dd8f95e1b2a355e87cf2a431c33326
Abraham Meza <ameza2@nevada.unr.edu>
2022-04-12 16:54:58 -0700

    Update AC_AddShoppingPage.java

--- AutoCart/app/src/main/java/com/example/autocart/AC_AddShoppingPage.java
+++ AutoCart/app/src/main/java/com/example/autocart/AC_AddShoppingPage.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +14,8 @@ import android.util.Log;
 
 public class AC_AddShoppingPage extends AppCompatActivity {
 
+    /* Variable Initialization */
+
     Button addButton;
 
     @Override
@@ -22,46 +23,48 @@ public class AC_AddShoppingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.autocart_addshoppingpage);
 
+        /* Fetch Page Activity */
+
         final EditText productEntry = findViewById(R.id.productName2);
         addButton = (Button)findViewById(R.id.addShopping);
 
+        /* Add Shopping Button */
+
         addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                boolean emptyProduct = productEntry.getText().toString().isEmpty(); // check for empty product name field
 
-                boolean emptyProduct = productEntry.getText().toString().isEmpty();
-
-//                Log.d("Empty Product? ", "" + emptyProduct);
-//                Log.d("Product Name: ", "" + productEntry.getText().toString());
-//                Log.d("Product Characters: ", "" + productEntry.getText().toString().length());
-
-                if (emptyProduct) {
+                if (emptyProduct) { // if statement: if product field is empty, prompt error message
                     Toast.makeText(getApplicationContext(), "Invalid Form Submission: Missing Product Name.", Toast.LENGTH_LONG).show(); // deactivation prompt
-                    Log.d("Error [1]: ", "Empty Text Field");
+                    //Log.d("Error [1]: ", "Empty Text Field");
                 }
-                else {
-                    Log.d("Success:", "Valid Text Fields");
+                else { // else statement: else if product field is not empty, save new entry into shopping list file
+                    //Log.d("Success:", "Valid Text Fields");
 
-                    if (!productEntry.getText().toString().isEmpty()) {
-                        File file = new File(AC_AddShoppingPage.this.getFilesDir(), "shopping");
-                        if (!file.exists()) {
-                            file.mkdir();
-                        }
-                        try {
-                            File gpxfile = new File(file, "list");
-                            FileWriter writer = new FileWriter(gpxfile, true);
-                            writer.write(productEntry.getText().toString() + "\n");
-                            writer.close();
-
-                            Toast.makeText(AC_AddShoppingPage.this, "Product Entry Saved", Toast.LENGTH_LONG).show();
-                        } catch (Exception e) {
-                        }
+                    // Export New Entry to File //
+                    File file = new File(AC_AddShoppingPage.this.getFilesDir(), "shopping"); // open shopping list directory
+                    if (!file.exists()) { // if statement: if shopping directory does not exist, create new directory
+                        file.mkdir(); // create shopping list directory
                     }
+                    try {
+                        File gpxfile = new File(file, "list"); // create shopping list file
+                        FileWriter writer = new FileWriter(gpxfile, true); // open shopping list file in append mode (add)
+                        writer.write(productEntry.getText().toString() + "\n"); // add new product entry
+                        writer.close(); // close shopping list file
+                        Toast.makeText(AC_AddShoppingPage.this, "Product Entry Saved", Toast.LENGTH_LONG).show(); // prompt successful confirmation
+                    } catch (Exception e) {
+                    }
+
+                    /* Redirect to Shopping Page */
+
                     Intent intent = new Intent(AC_AddShoppingPage.this, AC_ShoppingPage.class);
                     startActivity(intent);
                 }
             }
         });
     }
+
+    /* Back Button Navigation */
 
     @Override
     public void onBackPressed() {
